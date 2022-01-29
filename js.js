1afe6ae069dafbd8e39512187b1cb9e45fef0daf
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-29 02:25:04 -0500

    I have mostly finished up the project. I am now planning on just fleshing it out a bit more.

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
 let gridSize = 16;
-
 let container = document.querySelector("#container");
+
+
+function addButtonInput(){
+    let clearBtn = document.querySelector('.clear');
+    let gridSizeBtn = document.querySelector('.size');
+    clearBtn.addEventListener('click', clearGrid);
+    gridSizeBtn.addEventListener('click', chooseGridSize);
+}
+
 function createGrid(size){
     for(let i = 0; i < size; i++){
         const row = document.createElement('div');
@@ -16,31 +24,8 @@ function createGrid(size){
             row.appendChild(square);
         }
     }
+    addListeners();
 }
-
-// choose grid size button
-
-let gridSizeBtn = document.querySelector('.size');
-gridSizeBtn.addEventListener('click', chooseGridSize);
-
-function chooseGridSize(){
-    let chosenGridSize = +prompt('Enter grid size (1-100): ');
-    chosenGridSize = Math.abs(chosenGridSize);
-    if (100 >= Math.abs(chosenGridSize) >= 1){
-        gridSize = chosenGridSize;
-        clearGrid();
-    }
-    else{
-        chooseGridSize();
-    }
-}
-
-createGrid(gridSize);
-
-// clear screen button
-
-let clearBtn = document.querySelector('.clear');
-clearBtn.addEventListener('click', clearGrid);
 
 function clearGrid(){
     while (container.firstChild) {
@@ -49,14 +34,29 @@ function clearGrid(){
     createGrid(gridSize);
 }
 
-// hover making randomized colors
+function chooseGridSize(){
+    let chosenGridSize = +prompt('Enter grid size (1-100): ');
+    chosenGridSize = Math.abs(chosenGridSize);
+    if (100 >= (chosenGridSize) >= 1){
+        gridSize = chosenGridSize;
+        clearGrid();
+    }
+    else{
+        chooseGridSize();
+    }
+}
 
-let squares = document.querySelectorAll('.square');
-Array.from(squares).forEach(function(e){
-    e.addEventListener('mouseover', () => {
-        randNum = Math.random() * 255;
-        randNum2 = Math.random() * 255;
-        randNum3 = Math.random() * 255;
-        e.style.backgroundColor = `rgb(${randNum}, ${randNum2}, ${randNum3})`;
+function addListeners(){
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(function(e){
+        e.addEventListener('mouseover', () => {
+            randNum = Math.random() * 255;
+            randNum2 = Math.random() * 255;
+            randNum3 = Math.random() * 255;
+            e.style.backgroundColor = `rgb(${randNum}, ${randNum2}, ${randNum3})`;
+        });
     });
-});
+}
+
+addButtonInput();
+createGrid(gridSize);
