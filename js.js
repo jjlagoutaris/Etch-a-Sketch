1fcbf89ae9f0be97f6a1259443e05e6a9cd5f2b6
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-29 03:15:26 -0500

    I added the color selector, set the default color to black, and then added a random colors button

--- js.js
+++ js.js
@@ -1,26 +1,30 @@
 let gridSize = 16;
+let randomColor = false;
+let currentColor = '#000000';
 let container = document.querySelector("#container");
 
 
 function addButtonInput(){
     let clearBtn = document.querySelector('.clear');
     let gridSizeBtn = document.querySelector('.size');
+    let colorSelector = document.querySelector('#colorpicker');
+    let randomBtn = document.querySelector('.randomized');
     clearBtn.addEventListener('click', clearGrid);
     gridSizeBtn.addEventListener('click', chooseGridSize);
+    colorSelector.addEventListener('input', changeColor);
+    randomBtn.addEventListener('click', randomize);
 }
 
 function createGrid(size){
     for(let i = 0; i < size; i++){
         const row = document.createElement('div');
-        row.classList.add('row');
+        row.className = 'row';
         container.appendChild(row);
         for (let j = 0; j < size; j++){
             const square = document.createElement('div');
             square.className = 'square';
-            let width = 958 / parseInt(size);
-            square.style.width = `${width}px`;
-            let height = 498 / parseInt(size);
-            square.style.height = `${height}px`;
+            square.style.width = `${958 / parseInt(size)}px`;
+            square.style.height = `${498 / parseInt(size)}px`;
             row.appendChild(square);
         }
     }
@@ -46,16 +50,37 @@ function chooseGridSize(){
     }
 }
 
+function changeColor(){
+    let chosenColor = document.getElementById('colorpicker').value;
+    randomColor = false;
+    currentColor = chosenColor;
+    addListeners();
+}
+
+function randomize(){
+    randomColor = true;
+    addListeners();
+}
+
 function addListeners(){
     let squares = document.querySelectorAll('.square');
-    squares.forEach(function(e){
-        e.addEventListener('mouseover', () => {
-            randNum = Math.random() * 255;
-            randNum2 = Math.random() * 255;
-            randNum3 = Math.random() * 255;
-            e.style.backgroundColor = `rgb(${randNum}, ${randNum2}, ${randNum3})`;
+    if (randomColor === true){
+        squares.forEach(function(e){
+            e.addEventListener('mouseover', () => {
+                randNum = Math.random() * 255;
+                randNum2 = Math.random() * 255;
+                randNum3 = Math.random() * 255;
+                e.style.backgroundColor = `rgb(${randNum}, ${randNum2}, ${randNum3})`;
+            });
         });
-    });
+    }
+    else{
+        squares.forEach(function(e){
+            e.addEventListener('mouseover', () => {
+                e.style.backgroundColor = currentColor;
+            });
+        });
+    } 
 }
 
 addButtonInput();
